4e7efe2852a1ba67b95c4f567774cdf53b85ea52
shubhanshuyadav <shubhanshuemailid@gmail.com>
2021-09-14 14:39:29 +0530

    version1 (basic functionality)

--- index.js
+++ index.js
@@ -1,77 +1,138 @@
 let questionSet=JSON.parse(data);
-
+let isAnswered=false;
+let noOfAnsweredQus=0;
 let currentQuestionIndex=0;
 let score=0;
 
-let startTimer=function(){
-    let minute=2;
+let timeFunction=function(){
+    document.getElementById('timer').style.display="unset";
+    let minute=0;
     let sec=60;
-    setInterval(function() {
-        document.getElementById("timer").innerHTML = minute + " : " + sec;
-        sec--;
-        if (sec == 00) {
-          minute --;
-          sec = 60;
-          if (minute == 0) {
-            showResult();
-          }
+    let startTimer=setInterval(function() {
+            document.getElementById("timer").innerHTML = minute + " : " + sec;
+            sec--;
+            if (minute == 0 && sec==0) {
+                document.getElementById("timer").innerHTML = minute + " : " + sec;
+                stopTimer();
+            }
+            if (sec == 00 && minute!=0) {
+              minute --;
+              sec = 60;  
+            }
+          }, 1000);
+      let stopTimer=function(){
+        showResult();
+        clearInterval(startTimer);
+    }
+}
+
+let showResult=function(){
+    for(let i=0;i<questionSet.length;i++){
+        if(questionSet[i].isAnsweredRight){
+         score++;
         }
-      }, 1000);
+    } 
+    document.getElementById('main-container').style.display="none";
+    document.getElementById('score-text').innerHTML="Your score is "+score;
+    document.getElementById("score-card").style.display="unset";
+    score=0;
+ }
+
+let getUserAnswer=function(){
+    let qusOptions=document.getElementsByName('qus-option');
+    for(let i=0;i<qusOptions.length;i++){
+        if(qusOptions[i].checked){
+            isAnswered=true;
+            return qusOptions[i].value;
+        }
+    }
+    return null;
+}
+
+let displayButtons=function(questionIndex){
+    if(questionIndex>0){
+        document.getElementById("prev-btn").style.display="unset";
+    }else{
+        document.getElementById("prev-btn").style.display="none"; 
+    }
+    if(questionIndex===questionSet.length-1){
+        document.getElementById("next-btn").style.display="none";
+        document.getElementById("submit-btn").style.display="unset";
+    }else{
+        document.getElementById("next-btn").style.display="unset";
+        document.getElementById("submit-btn").style.display="none";
+    } 
 }
 
 let displayQuestion =function(questionIndex){
         if(questionIndex<questionSet.length && questionIndex>=0){
             document.getElementById("question").innerHTML=questionSet[questionIndex].question;
             let options=questionSet[questionIndex].options
-            let answer=questionSet[questionIndex].answer;
             for(let option in options ){
-            document.getElementById(option).innerHTML=options[option];
-                 }
+                document.getElementById(option).innerHTML=options[option];
+            }
         }
-        if(questionIndex>0){
-            document.getElementById("prev-btn").style.display="unset";
-        }else{
-            document.getElementById("prev-btn").style.display="none"; 
-        }
-        if(questionIndex===questionSet.length-1){
-            document.getElementById("next-btn").style.display="none";
-        }else{
-            document.getElementById("next-btn").style.display="unset";
-        }
+        displayButtons(questionIndex)        
 }
 
-
-let getUserAnswer=function(){
-    let qusOptions=document.getElementsByName('qus-option');
-    for(let i=0;i<qusOptions.length;i++){
-        if(qusOptions[i].checked){
-            return qusOptions[i].value;
-        }
-    }
-}
-
-let setScore=function(){
-
-}
-
-let showResult=function(){
-    
-}
-
-
-let startQuiz=function(){
-    document.getElementById('welcome-screen').style.display="none";
-    document.getElementById('main-container').style.display="unset";
-    displayQuestion(currentQuestionIndex);
+let buttonEventLisners=function(currentQuestionIndex){
     document.getElementById("next-btn").addEventListener("click",function(){
+        let answer=questionSet[currentQuestionIndex].answer;
+        let answerByUser=getUserAnswer();
+        if(answerByUser){
+            noOfAnsweredQus++;
+        }
+        if(answerByUser===answer){
+            questionSet[currentQuestionIndex].isAnsweredRight=true;
+        }else{
+            questionSet[currentQuestionIndex].isAnsweredRight=false;
+        }
         currentQuestionIndex++;
         displayQuestion(currentQuestionIndex);
     });
 
     document.getElementById("prev-btn").addEventListener("click",function(){
+        noOfAnsweredQus--;
         currentQuestionIndex--;
         displayQuestion(currentQuestionIndex);
     });
+
+    document.getElementById("submit-btn").addEventListener("click",function(){
+        let answer=questionSet[currentQuestionIndex].answer;
+        let answerByUser=getUserAnswer();
+        if(answerByUser){
+            noOfAnsweredQus++;
+        }
+        if(answerByUser===answer){
+            questionSet[currentQuestionIndex].isAnsweredRight=true;
+        }else{
+            questionSet[currentQuestionIndex].isAnsweredRight=false;
+        }
+        showResult();
+    },{once : true});
+
+    document.getElementById("quit-btn").addEventListener("click",function(){
+        let answer=questionSet[currentQuestionIndex].answer;
+        let answerByUser=getUserAnswer();
+        if(answerByUser){
+            noOfAnsweredQus++;
+        }
+        if(answerByUser===answer){
+            questionSet[currentQuestionIndex].isAnsweredRight=true;
+        }else{
+            questionSet[currentQuestionIndex].isAnsweredRight=false;
+        }
+        showResult();
+    },{once : true});
+}
+
+let startQuiz=function(){
+    timeFunction();
+    document.getElementById('welcome-screen').style.display="none";
+    document.getElementById('score-card').style.display="none";
+    document.getElementById('main-container').style.display="unset";
+    displayQuestion(currentQuestionIndex);
+    buttonEventLisners(currentQuestionIndex); 
 }
 
 
